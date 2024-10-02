3f4000537ae3e213f6e4cf452774d134e2002000
vikas-reddy-319 <vikasreddynagulapelly319@gmail.com>
2024-10-02 15:56:00 -0400

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mental Health Assistant',
+    version= '0.0.0',
+    author= 'Vikas Reddy',
+    author_email= 'vikasreddynagulapelly319@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
